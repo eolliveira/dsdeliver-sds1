f9221a0e67fde0fb4ce5eaa1db05ed140b7727c1
Erick Oliveira <erickoliveira@rorato.local>
2021-12-20 10:21:04 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsdeliver.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	
-	List<Product>findAllByOrderByNameAsc();
+	List<Product> findAllByOrderByNameAsc();
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,8 +1,27 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repositories.OrdertRepository;
 
 @Service
 public class OrderService {
+
+	@Autowired
+	private OrdertRepository repository;
+
+	@Transactional
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findAll();
+		return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
+	}
 
 }
